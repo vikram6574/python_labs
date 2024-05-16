19295f101e3c7fe0da11cd97286a7d9b8f87e803
Vikram Mehta <vikram6574@gmail.com>
2024-05-16 18:13:28 -0500

    odd_even_test

---
+++ chk_odd_even.py
@@ -0,0 +1,11 @@
+# if statement script to check if number is even or odd
+
+print ("Please enter a number to check if number is even or odd")
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd") 
+    
+
+
